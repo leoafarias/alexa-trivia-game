68e55c4d3744e9777d7c017802f1a669254b9a2c
Leo Farias <leokcta@gmail.com>
2017-12-10 01:54:23 -0500

    Tweaked messages, and added audio files during QA

--- src/messages.js
+++ src/messages.js
@@ -20,19 +20,24 @@ module.exports = {
       TRIVIA_UNHANDLED: "Try saying a number between 1 and %s",
       HELP_UNHANDLED: "Say yes to continue, or no to end the game.",
       START_UNHANDLED: "Say start to start a new game.",
-      NEW_GAME_MESSAGE: "Welcome to %s. ",
+      NEW_GAME_MESSAGE: "Welcome to %s",
       WELCOME_MESSAGE:
         "I will ask you %s questions, try to get as many right as you can. " +
-        "Just say the number of the answer. Let's begin. ",
+        "Just say the number of the answer. Let's begin.  <say-as interpret-as='interjection'>good luck</say-as>",
       ANSWER_CORRECT_MESSAGE: "correct. ",
+      ANSWER_CORRECT_SOUND:
+        '<audio src="https://d2kiyj3ex6fvqh.cloudfront.net/right-answer.mp3" /> ',
       ANSWER_WRONG_MESSAGE: "wrong. ",
+      ANSWER_WRONG_SOUND:
+        '<audio src="https://d2kiyj3ex6fvqh.cloudfront.net/wrong-answer.mp3" /> ',
       CORRECT_ANSWER_MESSAGE: "The correct answer is %s: %s. ",
       ANSWER_IS_MESSAGE: "That answer is ",
-      TELL_QUESTION_MESSAGE: "Question %s. %s ",
+      TELL_QUESTION_MESSAGE:
+        'Question %s. <audio src="https://d2kiyj3ex6fvqh.cloudfront.net/stinger.mp3" />, %s ',
       GAME_OVER_MESSAGE:
         "You got %s out of %s questions correct. Thank you for playing!",
       SCORE_IS_MESSAGE: "Your score is %s. ",
-      WELCOME_BACK: "Hey %s, welcome back",
+      WELCOME_BACK: "Hey %s. Welcome back. ",
       RESUME_GAME:
         "I see you already have a game that you did not complete. Let's continue where you left off! ",
       ALREADY_COMPLETED_MESSAGE:
